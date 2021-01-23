79594f2cb546128a6d5979f60ded657fe541a38f
Dima Gerasimov <karlicoss@gmail.com>
2021-01-23 09:18:19 +0000

    use dev version of 2D graph for easier debugging

--- src/main/frontend/extensions/graph_2d.cljs
+++ src/main/frontend/extensions/graph_2d.cljs
@@ -20,11 +20,16 @@
            (if @graph-component
              (reset! *loading? false)
              (do
+               ;; ok, at least I didn't have to manually remove the component thing?
+               ;; yarn add react-force-graph-3d
+               ;; cp node_modules/react-force-graph-2d/dist/react-force-graph-2d.js static/js/
+               ;; (for some reason doesn't load directly from node_modules??)
                (loader/load
-                (config/asset-uri "/static/js/react-force-graph.min.js")
+                (config/asset-uri "/static/js/react-force-graph-2d.js")
                 (fn []
                   (reset! graph-component
-                          (r/adapt-class (gobj/get js/window.ForceGraph "ForceGraph2D")))
+                          ;; NOTE: need to hard refresh page (Ctrl-F5)
+                          (r/adapt-class js/window.ForceGraph2D))
                   (reset! *loading? false)))))
            state)}
   [opts]
